275995d8add839ea5f5795e0e63124ab29ab1b9f
gjf2a <ferrer@hendrix.edu>
2022-03-06 16:20:57 -0600

    Added basic_midi_loop.

--- my_funcs.rb
+++ my_funcs.rb
@@ -4,6 +4,10 @@ define :cool_tri do |note|
   synth :tri, note: note, attack: 0.2, attack_level: 1, decay: 0.2, sustain_level: 0.4, sustain: 0.4, release: 0.2
 end
 
+define :blade_swell do |note, amp|
+  synth :blade, note: note, amp: amp, attack: 0.5, attack_level: 1, decay: 0.2, sustain_level: 0.4, sustain: 0.4, release: 0.4
+end
+
 
 define :play_melody do |note_times_list, note_maker|
   note_times_list.length().times do |index|
@@ -22,5 +26,13 @@ define :downshift do |notes, n, sc|
   end
   
   return down
+end
+
+define :basic_midi_loop do |note_maker|
+  live_loop :midi_fun do
+    use_real_time
+    note, velocity = sync "/midi:i2m_musicport:1/note_on"
+    method(note_maker).call(note, velocity / 127.0)
+  end
 end
 
