4c880fe2a8d1f2901c0ec9efff98c5a9f2434cbf
gjf2a <ferrer@hendrix.edu>
2022-03-06 16:07:15 -0600

    Removed functions from Joy to the World; using run_file to import them.

--- joy_to_the_world.rb
+++ joy_to_the_world.rb
@@ -1,28 +1,6 @@
 # Welcome to Sonic Pi
 
-define :cool_tri do |note|
-  synth :tri, note: note, attack: 0.2, attack_level: 1, decay: 0.2, sustain_level: 0.4, sustain: 0.4, release: 0.2
-end
-
-
-define :play_melody do |note_times_list, note_maker|
-  note_times_list.length().times do |index|
-    method(note_maker).call note_times_list[index][0]
-    sleep note_times_list[index][1]
-  end
-end
-
-
-define :downshift do |notes, n, sc|
-  down = []
-  notes.length().times do |index|
-    scale_index = sc.index(notes[index][0])
-    scaled_note = [sc[scale_index - n], notes[index][1]]
-    down.append(scaled_note)
-  end
-  
-  return down
-end
+run_file "~/Documents/sonic_pi_songs/my_funcs.rb"
 
 
 part_1 = [[74, 0.5],   [73, 0.375], [71, 0.125], [69, 0.75],
