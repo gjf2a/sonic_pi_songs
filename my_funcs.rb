d7903b151242787ebc65f92e0eba2408a13c6232
gjf2a <ferrer@hendrix.edu>
2022-03-06 17:35:57 -0600

    Added more sounds.

--- my_funcs.rb
+++ my_funcs.rb
@@ -1,13 +1,24 @@
 # Welcome to Sonic Pi
 
+define :basic_tri do |note, amp=1|
+  synth :tri, note: note, amp: amp
+end
+
 define :cool_tri do |note, amp=1|
   synth :tri, note: note, amp: amp, attack: 0.2, attack_level: 1, decay: 0.2, sustain_level: 0.4, sustain: 0.4, release: 0.2
 end
 
-define :blade_swell do |note, amp|
+define :blade_swell do |note, amp=1|
   synth :blade, note: note, amp: amp, attack: 0.5, attack_level: 1, decay: 0.2, sustain_level: 0.4, sustain: 0.4, release: 0.4
 end
 
+define :additive_1 do |note, amp=1|
+  with_fx :flanger do
+    synth :sine, note: note, amp: amp
+    synth :square, note: note, amp: amp
+    synth :tri, note: note + 12, amp: amp * 0.4
+  end
+end
 
 define :play_melody do |note_times_list, note_maker|
   note_times_list.length().times do |index|
@@ -36,3 +47,17 @@ define :basic_midi_loop do |note_maker|
   end
 end
 
+define :midi_cutoff_loop do |note_maker|
+  sound = nil
+  live_loop :midi_games do
+    use_real_time
+    value = sync "/midi:i2m_musicport:1/*"
+    if value.length() == 2
+      if not sound == nil
+        control sound, note: 0
+        control sound, amp: 0
+      end
+      sound = method(note_maker).call(value[0], value[1] / 127.0)
+    end
+  end
+end
