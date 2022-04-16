1325268fac1ea906aebae9eff9be68042fbb93dd
gjf2a <ferrer@hendrix.edu>
2022-04-16 13:35:35 -0500

    Removed many superflous files.

--- midi1.rb
+++
@@ -1,9 +0,0 @@
-# Welcome to Sonic Pi
-
-# Based on Tutorial 11.1
-
-live_loop :midi_piano do
-  use_real_time
-  note, velocity = sync "/midi:*/note_on"
-  synth :piano, note: note, amp: velocity / 127.0
-end
